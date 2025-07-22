9f5616a915650b44bd6fa9744406ade606027400
Maria Clara Ferreira Lopes <159304814+mariaclaraferr2007@users.noreply.github.com>
2025-07-21 22:03:01 -0300

    Add files via upload

---
+++ FitNerd/app/src/main/java/com/example/fitnerd/MainActivity.kt
@@ -0,0 +1,39 @@
+
+package com.example.fitnerd
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnCadastrar = findViewById<Button>(R.id.btn_cadastrar)
+        btnCadastrar.setOnClickListener{
+            val nome = binding.edtNome.text.toString()
+
+            Log.d("INFO_CLIENTE", "NOME: ${nome}")
+        }
+    }
+}
